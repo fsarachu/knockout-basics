23bb1168f6a4d56bc13ae2e6167dd6fcbdc7612d
fsarachu <fsarachu@gmail.com>
2017-02-19 07:35:32 -0300

    Consume generated sample data... This webpack thing is working sooo well

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,5 @@
-import Product from './models/Product.js';
+import sampleData from './data.js';
 
-console.log('Hello World!');
-
-var products = [];
-
-products.push(new Product({title: 'First Product', price: 12.99}));
-products.push(new Product({title: 'Second Product', price: 8.50}));
-products.push(new Product({title: 'Third Product', price: 1.99}));
-products.push(new Product({title: 'Fourth Product', price: 5.00}));
-
-products.forEach(function (product) {
+sampleData.products.forEach(function (product) {
   console.log(`${product.id}: ${product.title} - $ ${product.price}`);
 });
-

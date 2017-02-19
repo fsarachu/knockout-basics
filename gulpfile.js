87540f784b251ef9924e0f819774e4768515efd2
fsarachu <fsarachu@gmail.com>
2017-02-19 07:03:18 -0300

    Add webpack to gulp 'scripts' task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const browserSync = require('browser-sync').create();
 const del = require('del');
 const wiredep = require('wiredep').stream;
 const runSequence = require('run-sequence');
+const webpack2 = require('webpack');
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
@@ -31,6 +32,7 @@ gulp.task('scripts', () => {
     .pipe($.plumber())
     .pipe($.if(dev, $.sourcemaps.init()))
     .pipe($.babel())
+    .pipe($.webpack(require('./webpack.config.js'), webpack2))
     .pipe($.if(dev, $.sourcemaps.write('.')))
     .pipe(gulp.dest('.tmp/scripts'))
     .pipe(reload({stream: true}));

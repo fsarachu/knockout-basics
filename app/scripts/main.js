6e1692cc78cbc735c89da726388ebfa5883d4ae6
fsarachu <fsarachu@gmail.com>
2017-02-19 07:06:41 -0300

    Add some test code for webpack and babel, seems to work right...

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+import Product from './models/Product.js';
+
+console.log('Hello World!');
+
+var products = [];
+
+products.push(new Product({title: 'First Product', price: 12.99}));
+products.push(new Product({title: 'Second Product', price: 8.50}));
+products.push(new Product({title: 'Third Product', price: 1.99}));
+products.push(new Product({title: 'Fourth Product', price: 5.00}));
+
+products.forEach(function (product) {
+  console.log(`${product.id}: ${product.title} - $ ${product.price}`);
+});
+

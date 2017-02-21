6f4fe19c81fcd01d050c1e8d3cc005be61327151
fsarachu <fsarachu@gmail.com>
2017-02-21 04:47:54 -0300

    Remove variable, use object property directly

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -2,9 +2,9 @@ import Product from './models/Product.js';
 
 var sampleData = {};
 
-var products = [];
+sampleData.products = [];
 
-products.push(new Product({
+sampleData.products.push(new Product({
   title: 'Aviator Classic Gold Sunglasses',
   brand: 'Ray-Ban',
   price: 161.06,
@@ -12,14 +12,14 @@ products.push(new Product({
   image: 'images/aviator_classic_gold_sunglasses.jpg'
 }));
 
-products.push(new Product({
+sampleData.products.push(new Product({
   title: 'Cable Minna Beanie',
   brand: 'The North Face',
   price: 5.99,
   image: 'images/cable_minna_beanie.jpg'
 }));
 
-products.push( new Product({
+sampleData.products.push(new Product({
   title: 'Preston Flap Wallet',
   brand: 'Fossil',
   price: 10.00,
@@ -27,7 +27,7 @@ products.push( new Product({
   image: 'images/preston_flap_wallet.jpg'
 }));
 
-products.push( new Product({
+sampleData.products.push(new Product({
   title: 'Floral Perforated Belt',
   brand: 'Fossil',
   price: 7.90,
@@ -35,7 +35,7 @@ products.push( new Product({
   image: 'images/floral_perforated_belt.jpg'
 }));
 
-products.push( new Product({
+sampleData.products.push(new Product({
   title: 'Fuzzy Earflap Tibetan Beanie',
   brand: 'The North Face',
   price: 5.00,
@@ -43,7 +43,7 @@ products.push( new Product({
   image: 'images/fuzzy_earflap_tibetan_beanie.jpg'
 }));
 
-products.push( new Product({
+sampleData.products.push(new Product({
   title: 'Junior\'s Cowgirl Hat',
   brand: 'Roxy',
   price: 8.99,
@@ -51,7 +51,7 @@ products.push( new Product({
   image: 'images/junior\'s_cowgirl_hat.jpg'
 }));
 
-products.push( new Product({
+sampleData.products.push(new Product({
   title: 'Reversible Belt',
   brand: 'Calvin Klein',
   price: 30.00,
@@ -59,7 +59,7 @@ products.push( new Product({
   image: 'images/reversible_belt.jpg'
 }));
 
-products.push( new Product({
+sampleData.products.push(new Product({
   title: 'Sun Child Wallet',
   brand: 'Roxy',
   price: 8.50,
@@ -67,7 +67,5 @@ products.push( new Product({
   image: 'images/sun_child_wallet.jpg'
 }));
 
-
-sampleData.products = products;
 
 export default sampleData;

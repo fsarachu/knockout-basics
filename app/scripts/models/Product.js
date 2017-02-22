66d1b2be88347e58e5a9dee10b88c2772f211563
fsarachu <fsarachu@gmail.com>
2017-02-22 05:42:22 -0300

    Product properties are now observables

--- app/scripts/models/Product.js
+++ app/scripts/models/Product.js
@@ -2,10 +2,10 @@ var id = 1;
 
 var Product = function (data) {
   this.id = id++;
-  this.title = data.title || '';
-  this.brand = data.brand || '';
-  this.price = data.price || 0;
-  this.discount = data.discount || 0;
+  this.title = ko.observable(data.title || '');
+  this.brand = ko.observable(data.brand || '');
+  this.price = ko.observable(data.price || 0);
+  this.discount = ko.observable(data.discount || 0);
 };
 
 export default Product;

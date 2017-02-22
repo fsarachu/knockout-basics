3d60bf08059a35b274b8a7cb031f0183b567cf8a
fsarachu <fsarachu@gmail.com>
2017-02-22 06:43:21 -0300

    Rename "price" property to "actualPrice"

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -7,7 +7,7 @@ sampleData.products = [];
 sampleData.products.push(new Product({
   title: 'Aviator Classic Gold Sunglasses',
   brand: 'Ray-Ban',
-  price: 161.06,
+  actualPrice: 161.06,
   discount: 40,
   image: 'images/aviator_classic_gold_sunglasses.jpg'
 }));
@@ -15,14 +15,14 @@ sampleData.products.push(new Product({
 sampleData.products.push(new Product({
   title: 'Cable Minna Beanie',
   brand: 'The North Face',
-  price: 5.99,
+  actualPrice: 5.99,
   image: 'images/cable_minna_beanie.jpg'
 }));
 
 sampleData.products.push(new Product({
   title: 'Preston Flap Wallet',
   brand: 'Fossil',
-  price: 10.00,
+  actualPrice: 10.00,
   discount: 0,
   image: 'images/preston_flap_wallet.jpg'
 }));
@@ -30,7 +30,7 @@ sampleData.products.push(new Product({
 sampleData.products.push(new Product({
   title: 'Floral Perforated Belt',
   brand: 'Fossil',
-  price: 7.90,
+  actualPrice: 7.90,
   discount: 0,
   image: 'images/floral_perforated_belt.jpg'
 }));
@@ -38,7 +38,7 @@ sampleData.products.push(new Product({
 sampleData.products.push(new Product({
   title: 'Fuzzy Earflap Tibetan Beanie',
   brand: 'The North Face',
-  price: 5.00,
+  actualPrice: 5.00,
   discount: 0,
   image: 'images/fuzzy_earflap_tibetan_beanie.jpg'
 }));
@@ -46,7 +46,7 @@ sampleData.products.push(new Product({
 sampleData.products.push(new Product({
   title: 'Junior\'s Cowgirl Hat',
   brand: 'Roxy',
-  price: 8.99,
+  actualPrice: 8.99,
   discount: 0,
   image: 'images/junior\'s_cowgirl_hat.jpg'
 }));
@@ -54,7 +54,7 @@ sampleData.products.push(new Product({
 sampleData.products.push(new Product({
   title: 'Reversible Belt',
   brand: 'Calvin Klein',
-  price: 30.00,
+  actualPrice: 30.00,
   discount: 25,
   image: 'images/reversible_belt.jpg'
 }));
@@ -62,7 +62,7 @@ sampleData.products.push(new Product({
 sampleData.products.push(new Product({
   title: 'Sun Child Wallet',
   brand: 'Roxy',
-  price: 8.50,
+  actualPrice: 8.50,
   discount: 0,
   image: 'images/sun_child_wallet.jpg'
 }));
--- app/scripts/models/Product.js
+++ app/scripts/models/Product.js
@@ -4,11 +4,11 @@ var Product = function (data) {
   this.id = id++;
   this.title = ko.observable(data.title || '');
   this.brand = ko.observable(data.brand || '');
-  this.price = ko.observable(data.price || 0);
+  this.actualPrice = ko.observable(data.actualPrice || 0);
   this.discount = ko.observable(data.discount || 0);
   this.image = ko.observable(data.image || '');
   this.salePrice = ko.computed(function () {
-    return this.discount() ? this.price() * (this.discount() / 100) : this.price();
+    return this.discount() ? this.actualPrice() * (this.discount() / 100) : this.actualPrice();
   }, this);
 };
 
